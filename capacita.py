1c4eb5c85d0509b197f7393105e7bfb3204e583e
Carrotlord <j.oliverchu@berkeley.edu>
2016-02-18 19:15:41 -0800

    Allow statement execution.

--- capacita.py
+++ capacita.py
@@ -1,3 +1,5 @@
+# Use Python 2, not Python 3
+
 test_code = """
 x = 5
 y = 15
@@ -8,17 +10,35 @@ operators = ['+=', '=']
 
 # TODO: finish this function
 def tokenize_statement(stmt):
-    tokens = []
     for i in range(len(stmt)):
         for op in operators:
             if stmt[i:i+len(op)] == op:
-                tokens.append([
+                return [
                     stmt[0:i].strip(),
                     op,
                     stmt[i+len(op):].strip()
-                ])
-                break
-    return tokens
+                ]
+    return []
+    
+def execute_statement(stmt, env):
+    tokens = tokenize_statement(stmt)
+    if tokens[1] == '=':
+        env.assign(tokens[0], convert_value(tokens[2]))
+    elif tokens[1] == '+=':
+        val = env.get(tokens[0])
+        env.assign(tokens[0], val + convert_value(tokens[2]))
+    
+def convert_value(val):
+    """
+    Given a value in string form, converts to an int, float, etc.
+    """
+    try:
+        return int(val)
+    except ValueError:
+        try:
+            return float(val)
+        except ValueError:
+            return None
 
 class Environment(object):
     def __init__(self):
@@ -30,16 +50,17 @@ class Environment(object):
     def assign(self, var_name, value):
         self.frames[-1][var_name] = value
         
+    def get(self, var_name):
+        return self.frames[-1][var_name]
+        
     def pop(self):
         return self.frames.pop()
 
 def main():
     env = Environment()
-    env.assign('g', 3)
-    env.new_frame()
-    env.assign('h', 15)
+    execute_statement('x = 3', env)
+    execute_statement('x+=7', env)
+    execute_statement('y=9.23', env)
     print(env.frames)
-    print(tokenize_statement("x=3"))
-    print(tokenize_statement("y += 56"))
         
 main()

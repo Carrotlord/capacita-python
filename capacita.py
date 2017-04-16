a54199135e0ed0fd51736f8d8a245733e6328b69
Carrotlord <j.oliverchu@berkeley.edu>
2017-04-16 02:13:22 -0700

    Implement simple if statement.

--- capacita.py
+++ capacita.py
@@ -235,17 +235,76 @@ def prepare_control_flow(lines):
     5: // do other thing
     6: 
     """
+    def find_end_statement(lines, start):
+        """
+        Returns the corresponding end-statement
+        for the given clause-opener (which can be an if-statement,
+        while-statement, etc.)
+        Skips over end statements that are part of nested clauses.
+        """
+        i = start
+        open_clauses = 0
+        openers = ['if ', 'while ', 'for ', 'repeat ', 'switch ']
+        while i < len(lines):
+            line = lines[i]
+            if line == 'end':
+                if open_clauses == 0:
+                    return i
+                else:
+                    open_clauses -= 1
+            else:
+                for opener in openers:
+                    if line.startswith(opener):
+                        open_clauses += 1
+                        break
+            i += 1
+        throw_exception('NoEndStatement', 'Corresponding end statement does not exist.')
+    def replace_labels(lines):
+        """
+        Replaces labels in jump directives with the actual
+        addresses being jumped to.
+        """
+        label_table = {}
+        i = 0
+        for line in lines:
+            if line.startswith(':label'):
+                label_table[line[6:]] = i
+                # Erase the label at this line:
+                lines[i] = ''
+            i += 1
+        i = 0
+        jumps = [':j ', ':jt ', ':jf ']
+        for line in lines:
+            for jump in jumps:
+                if line.startswith(jump + 'label'):
+                    label_num = line[len(jump) + 5:]
+                    if label_num in label_table:
+                        lines[i] = jump + str(label_table[label_num])
+                        break
+            i += 1
+        return lines
     i = 0
+    label_counter = 0
     while i < len(lines):
         line = lines[i]
         if line.startswith('when '):
             lines[i : i+1] = [':cond ' + line[5:], ':jf ' + str(i + 3)]
+        elif line.startswith('if '):
+            # TODO : allow for complex if-statements that have else or
+            # else-if clauses
+            lines[i : i+1] = [':cond ' + line[3:], ':jf label' + str(label_counter)]
+            # Find the next end statement:
+            end = find_end_statement(lines, i + 2)
+            lines[end] = ':label' + str(label_counter)
+            label_counter += 1
         i += 1
+    lines = replace_labels(lines)
     return lines
 
 def execute_program(prgm):
     """Executes a program given as a string."""
     lines = prgm.split('\n')
+    lines = [line.strip() for line in lines]
     lines = prepare_control_flow(lines)
     env = Environment()
     prgm_counter = 0

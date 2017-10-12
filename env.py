9dd33f68118917e509bc2e62cb6b281dfa21de63
Carrotlord <j.oliverchu@gmail.com>
2017-10-12 05:59:58 -0700

    Enforce type restrictions.

--- env.py
+++ env.py
@@ -1,6 +1,7 @@
 from exception import throw_exception
 from builtin_function import int_div, make_list
 from execution import eval_parentheses
+from type_tree import value_is_a
 
 import re
 
@@ -17,13 +18,44 @@ class Environment(object):
         self.frames.append({})
     
     def assign(self, var_name, value):
-        match_obj = re.match(r'(\$?[A-Za-z_][A-Za-z_0-9]*)\[(.+)\]', var_name)
+        # Check for a type restriction
+        match_obj = re.match(r'([A-Za-z_][A-Za-z_0-9]*) (\$?[A-Za-z_][A-Za-z_0-9]*)', var_name)
         if match_obj:
-            indexable = match_obj.group(1)
-            index = eval_parentheses(match_obj.group(2), self)
-            self.frames[-1][indexable][index] = value
+            kind = match_obj.group(1)
+            if not value_is_a(value, kind):
+                throw_exception('MismatchedType', str(value) + ' is not of type ' + kind)
+            var = match_obj.group(2)
+            if var in self.frames[-1]:
+                type_tuple = self.frames[-1][var]
+                if type(type_tuple) is tuple:
+                    throw_exception('AlreadyRestrictedType', var + ' already has type ' + type_tuple[0])
+                else:
+                    throw_exception('AlreadyAnyType',
+                        var + ' already has type Any and cannot be further restricted.')
+            self.frames[-1][var] = (kind, value)
         else:
-            self.frames[-1][var_name] = value
+            # Is this an assignment to an index?
+            match_obj = re.match(r'(\$?[A-Za-z_][A-Za-z_0-9]*)\[(.+)\]', var_name)
+            if match_obj:
+                indexable = match_obj.group(1)
+                index = eval_parentheses(match_obj.group(2), self)
+                container = self.frames[-1][indexable]
+                if type(container) is tuple:
+                    # Modify the iterable contained within the tuple
+                    iterable = container[1]
+                    iterable[index] = value
+                else:
+                    # This iterable has no type restriction
+                    self.frames[-1][indexable][index] = value
+            else:
+                # No type restriction, no index
+                if var_name in self.frames[-1]:
+                    kind = self.frames[-1][var_name][0]
+                    if not value_is_a(value, kind):
+                        throw_exception('MismatchedType', str(value) + ' is not of type ' + kind)
+                    self.frames[-1][var_name] = (kind, value)
+                else:
+                    self.frames[-1][var_name] = value
     
     def update(self, var_name, value):
         if var_name in self.frames[-1]:
@@ -35,7 +67,10 @@ class Environment(object):
         i = -1
         while i >= -len(self.frames):
             if var_name in self.frames[i]:
-                return self.frames[i][var_name]
+                value = self.frames[i][var_name]
+                if type(value) is tuple:
+                    return value[1]
+                return value
             else:
                 i -= 1
         throw_exception('UndefinedVariable', var_name + ' is not defined.')
